51bba675f89357fa69389cef74ccd22b00c334db
Martin Lindström <martin.lindstrom94@gmail.com>
2022-05-06 23:17:14 +0200

    Generate a file that plots elevation and speed

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,18 @@ use std::io::BufReader;
 use gpx::read;
 use gpx::Gpx;
 
+mod error;
 mod plotting;
 
-const OUT_FILE_NAME: &'static str = "plotters-doc-data/sample.png";
+const OUT_FILE_NAME: &'static str = "plotters-doc-data/lundaloppet.png";
 
 fn main() {
     // This XML file actually exists — try it for yourself!
-    let file = File::open("test.GPX").unwrap();
+    let file = File::open("lundaloppet-2022-babel.gpx").unwrap();
     let reader = BufReader::new(file);
 
     // read takes any io::Read and gives a Result<Gpx, Error>.
-    let gpx: Gpx = read(reader).unwrap();
+    let mut gpx: Gpx = read(reader).unwrap();
 
-    plotting::create_plot(&gpx, OUT_FILE_NAME).unwrap();
+    plotting::create_plot(&mut gpx, OUT_FILE_NAME).unwrap();
 }
--- src/plotting.rs
+++ src/plotting.rs
@@ -3,31 +3,91 @@ use std::error::Error;
 use gpx::{Gpx, Track, TrackSegment};
 use plotters::prelude::*;
 
-pub fn create_plot(gpx: &Gpx, output_file: &str) -> Result<(), Box<dyn Error>> {
-    // Each GPX file has multiple "tracks", this takes the first one.
+use crate::error::GpxError;
+
+// TODO: Remove all the unwraps.
+pub fn create_plot(gpx: &mut Gpx, output_file: &str) -> Result<(), Box<dyn Error>> {
+    if gpx.tracks.len() != 1 {
+        return Err(Box::new(GpxError::new(
+            "GPX file must have exactly one track contained in it",
+        )));
+    }
     let track: &Track = &gpx.tracks[0];
 
-    // Each track will have different segments full of waypoints, where a
-    // waypoint contains info like latitude, longitude, and elevation.
     let segment: &TrackSegment = &track.segments[0];
     let num_points: usize = segment.points.len();
+
     let elevation: Vec<f64> = (0..num_points)
         .map(|i| segment.points[i].elevation.unwrap())
         .collect();
     let elevation = lowpass_filter(&elevation, 75);
 
-    let drawing_area = BitMapBackend::new(output_file, (1024, 768)).into_drawing_area();
+    let speed: Vec<f64> = (0..num_points)
+        .map(|i| segment.points[i].speed.unwrap())
+        .collect();
+    let speed = lowpass_filter(&speed, 75);
 
-    drawing_area.fill(&WHITE).unwrap();
+    let height: u32 = 768;
+    let width: u32 = 1024;
+    let root = BitMapBackend::new(output_file, (width, height)).into_drawing_area();
+    root.fill(&WHITE)?;
 
-    let mut chart = ChartBuilder::on(&drawing_area)
-        .build_cartesian_2d(0..num_points, -300.0..300.0)
-        .unwrap();
+    let (upper, lower) = root.split_vertically(height / 2);
+
+    // Plot elevation.
+
+    let plot_max = elevation
+        .iter()
+        .fold(f64::NEG_INFINITY, |x, &y| if x > y { x } else { y });
+
+    let mut chart = ChartBuilder::on(&upper)
+        .caption("Elevation", ("sans-serif", (10).percent_height()))
+        .set_label_area_size(LabelAreaPosition::Left, (15).percent())
+        .set_label_area_size(LabelAreaPosition::Bottom, (10).percent())
+        .margin((1).percent())
+        .build_cartesian_2d(0..num_points, -10.0..plot_max + 10.0)?;
+
+    let color = Palette99::pick(4).mix(0.9);
 
     chart
         .draw_series(LineSeries::new(
             (0..num_points).map(|i| (i, elevation[i])),
-            &RED,
+            color.stroke_width(2),
+        ))
+        .unwrap();
+
+    chart
+        .configure_mesh()
+        .x_desc("Time (TODO)") // TODO: Fix plotting of time.
+        .y_desc("Elevation (m)")
+        .draw()?;
+
+    // Plot speed.
+
+    let plot_max = speed
+        .iter()
+        .fold(f64::NEG_INFINITY, |x, &y| if x > y { x } else { y });
+
+    let mut chart = ChartBuilder::on(&lower)
+        .caption("Speed", ("sans-serif", (10).percent_height()))
+        .set_label_area_size(LabelAreaPosition::Left, (15).percent())
+        .set_label_area_size(LabelAreaPosition::Bottom, (10).percent())
+        .margin((1).percent())
+        .build_cartesian_2d(0..num_points, 1.0..plot_max + 4.0)?;
+
+    let color = Palette99::pick(3).mix(0.9);
+
+    chart
+        .configure_mesh()
+        .x_desc("Time (TODO)") // TODO: Fix plotting of time.
+        .y_desc("Speed (m/s)")
+        .draw()
+        .unwrap();
+
+    chart
+        .draw_series(LineSeries::new(
+            (0..num_points).map(|i| (i, speed[i])),
+            color.stroke_width(2),
         ))
         .unwrap();
 
